97cf89fcf503e526553e941c72c8f0835ead9c5e
Faye Skeen <fskeen@gmail.com>
2019-09-02 21:07:19 -0700

    first GET request done

--- README.md
+++ README.md
@@ -80,12 +80,16 @@ When the client makes a `POST` request to `/api/users`:
   - respond with HTTP status code `500` (Server Error).
   - return the following JSON object: `{ error: "There was an error while saving the user to the database" }`.
 
+
+
 When the client makes a `GET` request to `/api/users`:
 
 - If there's an error in retrieving the _users_ from the database:
   - cancel the request.
   - respond with HTTP status code `500`.
   - return the following JSON object: `{ error: "The users information could not be retrieved." }`.
+
+
 
 When the client makes a `GET` request to `/api/users/:id`:
 
@@ -99,6 +103,8 @@ When the client makes a `GET` request to `/api/users/:id`:
   - respond with HTTP status code `500`.
   - return the following JSON object: `{ error: "The user information could not be retrieved." }`.
 
+
+
 When the client makes a `DELETE` request to `/api/users/:id`:
 
 - If the _user_ with the specified `id` is not found:
@@ -110,6 +116,8 @@ When the client makes a `DELETE` request to `/api/users/:id`:
   - cancel the request.
   - respond with HTTP status code `500`.
   - return the following JSON object: `{ error: "The user could not be removed" }`.
+
+
 
 When the client makes a `PUT` request to `/api/users/:id`:
 
--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // implement your API here
+const express = require('express');
+
+const Users = require('./data/db.js')
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello world!! This is the home page.');
+});
+
+server.get(`/api/users`, (req, res) => {
+    // Returns an array of all the user objects contained in the database.
+    Users.find()
+        .then((users) => {
+            res.status(200).json(users)
+        })
+        .catch(() => {
+            res.status(500).json({
+                errorMessage: 'The user information could not be retrieved.'
+            });
+        });
+});
+
+server.post(`/api/users`, (req, res) => {
+    // Creates a user using the information sent inside the `request body`.
+
+    res.status(200).json(hobbits)
+})
+
+server.get(`/api/users/:id`, (req, res) => {
+    // Returns the user object with the specified `id`. 
+
+    res.status(200).json(hobbits)
+})
+
+server.put(`/api/users/:id`, (req, res) => {
+    // Updates the user with the specified `id` using data from the `request body`. Returns the modified document, **NOT the original**. |
+
+    res.status(200).json(hobbits)
+})
+
+server.delete(`/api/users/:id`, (req, res) => {
+    // Removes the user with the specified `id` and returns the deleted user. 
+
+    res.status(200).json(hobbits)
+})
+
+
+server.listen(8000, () => console.log("Server listening on port 8000"));
